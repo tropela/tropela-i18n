9a1ffcf7ddb7741d8d9782f5b5a572a3498d2b7a
Eneko Illarramendi <eneko.illarramendi@ugent.be>
2023-10-29 22:40:40 +0100

    feat(it): Italian translations, courtesy of Copilot

--- it/index.ts
+++ it/index.ts
@@ -1,5 +1,239 @@
 //! tropela-app translation file
 //! locale : Italian [it]
+//! author : github : https://github.com/github
 
 export default {
+  welcome:
+    'Tropela.eus è un gioco di ciclismo fantasy dove puoi giocare con i tuoi amici e familiari, gratuitamente, nelle gare più emozionanti della stagione.',
+  menu: {
+    home: 'Home',
+    races: 'Gare',
+    rules: 'Regole',
+    premium: 'Primi gregari',
+    blog: 'Blog',
+    store: 'Negozio',
+    about: 'Chi siamo',
+    privacy: 'Informativa sulla privacy',
+    terms: 'Termini di utilizzo',
+    contact: 'Contatti',
+    tropeltxos: 'Tropeltxos',
+  },
+  auth: {
+    login: {
+      title: 'Bentornato.',
+      link: 'Hai già un account? Accedi',
+    },
+    signup: {
+      title: 'Unisciti al gruppo.',
+      link: 'Non hai ancora un account? Registrati',
+    },
+    passwordReset: {
+      title: 'Reimposta la password.',
+      intro: 'Hai dimenticato la password? Inserisci il tuo indirizzo email e ti invieremo un link per reimpostarla.',
+      link: 'Hai dimenticato la password?',
+    },
+  },
+  countdown: {
+    months: 'mese | mesi',
+    days: 'giorno | giorni',
+    hours: 'ora | ore',
+    minutes: 'minuto | minuti',
+    seconds: 'secondo | secondi',
+  },
+  season: {
+    menu: {
+      standings: 'Classifica stagione',
+    },
+    collection: 'Collezione | Collezioni',
+    race: 'Gara | Gare',
+    racesCount: 'Nessuna gara | Una gara | {n} gare',
+  },
+  race: {
+    menu: {
+      presentation: 'Presentazione',
+      standings: 'Classifiche',
+      points: 'Punti ciclisti',
+      dorsals: 'Elenco ciclisti',
+      news: 'Notizie',
+      createPorra: 'Crea una squadra',
+      myPorra: 'La mia squadra',
+    },
+    canceled: 'Annullato',
+    stage: 'Tappa | Tappe',
+    team: 'Squadra | Squadre',
+    dorsal: {
+      filter: 'Filtra per nome, squadra o paese',
+      group: 'Gruppo | Gruppi',
+      status: 'Stato | Stati',
+      statuses: {
+        unconfirmed: 'Non confermato',
+        participating: 'Partecipante',
+        notParticipating: 'Non partecipante',
+        retired: 'Ritirato',
+      },
+      sort: {
+        alpha: 'Ordina per nome',
+        percent: 'Ordina per percentuale',
+        position: 'Ordina per punti',
+      },
+    },
+    porra: {
+      registrationOpens: 'Le registrazioni aprono in',
+      registrationOpenFor: 'Crea la tua squadra in',
+      showAllStages: 'Mostra tutte le tappe',
+    },
+    porrasCount: 'Nessuna squadra | Una squadra | {n} squadre',
+  },
+  place: {
+    menu: {
+      standings: 'Classifiche locali',
+    },
+  },
+  game: {
+    menu: {
+      standings: 'Classifiche del gioco',
+    },
+    banner: 'Questa è una gara sponsorizzata, con bei premi per gli utenti di Tropela.',
+    legal: 'Basi legali',
+    prizes: 'Premi',
+  },
+  user: {
+    menu: {
+      admin: 'Area amministrativa',
+      avatar: 'Ciclista',
+      dashboard: 'Dashboard',
+      privacy: 'Privacy',
+      profile: 'Profilo',
+      settings: 'Impostazioni',
+      settingsAndPrivacy: 'Impostazioni e privacy',
+      notifications: 'Notifiche',
+      subscriptions: 'Abbonamenti',
+      login: 'Accedi',
+      logout: 'Esci',
+      passwordChange: 'Cambia password',
+      emails: 'Email',
+      socialConnections: 'Connessioni social',
+      signup: 'Registrati',
+      standings: 'Le mie classifiche',
+      tropeltxos: 'I miei tropeltxos',
+    },
+    confirm: {
+      logout: 'Sei sicuro di voler uscire?',
+    },
+    tropeltxo: 'Tropeltxo | Tropeltxos',
+  },
+  ui: {
+    form: {
+      actions: {
+        change: 'Cambia',
+        delete: 'Elimina',
+        update: 'Aggiorna',
+        reset: 'Resetta',
+        save: 'Salva',
+        submit: 'Invia',
+        typeMessage: 'Scrivi il tuo messaggio',
+        login: 'Accedi',
+        signup: 'Registrati',
+      },
+      fields: {
+        username: 'Nome utente',
+        password: 'Password',
+        password2: 'Password (ripeti)',
+        email: 'Email',
+        firstName: 'Nome',
+        lastName: 'Cognome',
+        city: 'Città',
+        flag: 'La tua bandiera',
+      },
+      hints: {
+        city: 'La tua città verrà utilizzata per mostrare le classifiche locali.',
+        flag: 'Quella mostrata nelle classifiche.',
+      },
+      required: 'Richiesto.',
+      rules: {
+        required: 'Il campo è obbligatorio.',
+      },
+      social: {
+        facebook: 'Continua con Facebook',
+        google: 'Continua con Google',
+        twitter: 'Continua con Twitter / X',
+      },
+    },
+    select: {
+      all: 'Tutto | Tutti',
+      aggregate: 'Aggregato',
+      locale: 'Lingua',
+      stage: 'Tappa',
+      finalStage: 'Finale',
+    },
+    nav: {
+      pageFirst: 'Prima pagina',
+      pagePrevious: 'Pagina precedente',
+      pageNext: 'Pagina successiva',
+      pageLast: 'Ultima pagina',
+    },
+    or: 'o',
+  },
+  message: {
+    loggedIn: 'Accesso effettuato come {username}',
+    loggedOut: 'Hai effettuato l\'uscita',
+    passwordResetRequested: 'È stata inviata una email per il ripristino della password',
+    porraCreated: 'La tua squadra è stata creata',
+    porraUpdated: 'La tua squadra è stata aggiornata',
+    userProfileUpdated: 'Il tuo profilo è stato aggiornato',
+  },
+  notice: {
+    noTropeltxoMessages: 'Non hai messaggi tropeltxo',
+    placeStandingsLogIn: 'Accedi per vedere le tue classifiche locali.',
+    placeStandingsNoPlace: 'Aggiorna le informazioni sulla tua città per vedere le classifiche locali.',
+    porraClosed: 'Non puoi creare una squadra per questa gara.',
+    porraCreateLogIn: 'Devi accedere per creare una squadra.',
+    xedapAds: 'Vuoi vedere qui i tuoi annunci? Contattaci!',
+  },
+  premium: {
+    banner: {
+      join: 'Diventa un Primo Domestico e goditi alcune funzionalità extra!',
+      noAds: 'Senza pubblicità!',
+    },
+    action: {
+      upgrade: 'Aggiorna',
+    },
+  },
+  privacy: {
+    shareOnSocialMedia: 'Condividi la tua attività sui social media',
+    shareOnSocialMediaDescription:
+    'Quando crei una squadra, condividila sui social media (se stai usando il login di Twitter o Facebook).',
+    makeProfilePublic: 'Rendi il tuo profilo pubblico',
+    makeProfilePublicDescription:
+    'Puoi rendere il tuo profilo pubblico, in modo che gli altri utenti possano vedere la tua storia e i risultati di Tropela.',
+    receiveEmails: 'Ricevi email da Tropela',
+    receiveEmailsDescription: 'Promemoria per creare una squadra, notizie, ecc.',
+    receiveNotifications: 'Ricevi notifiche push',
+    receiveNotificationsDescription: 'Ricevi notifiche push sui dispositivi mobili, quando apriamo un nuovo gioco.',
+  },
+  xedap: {
+    banner: {
+    updateRider:
+      'Aggiorna il tuo pilota nelle impostazioni. Centinaia di diverse combinazioni per creare il tuo avatar unico, migliaia se sei un Primo Domestico!',
+    },
+    form: {
+      height: {
+        label: 'Altezza',
+      },
+      weight: {
+        label: 'Peso',
+      },
+      skin: {
+        label: 'Tonalità della pelle',
+      },
+      bikes: {
+        label: 'Bici',
+        options: {
+        bike_road: 'Strada',
+        bike_victorian: 'Vittoriano',
+        bike_mountain: 'MTB',
+        },
+      },
+    },
+  },
 };
