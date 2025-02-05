9b36b81f557e33c7208a02ce5897245b43ad38c0
Eneko Illarramendi <eneko@illarra.com>
2025-02-05 07:06:47 +0100

    feat(ast,gl): initial translations

--- ast/index.ts
+++ ast/index.ts
@@ -1,6 +1,317 @@
 //! tropela-app translation file
 //! locale : Asturian [ast]
+//! author : github : https://github.com/github
 
 export default {
   updated: 'Hemos actualizáu la aplicación. Refresca pa usar la última versión.',
+  welcome: 'En Tropela.eus pues competir colos tos familiares y amigos nes meyores carreres ciclistes de la temporada.',
+  menu: {
+    home: 'Entamu',
+    races: 'Competiciones',
+    seasons: 'Temporadas',
+    rules: 'Regles',
+    premium: 'Gregariu de lluxu | Gregarios de lluxu',
+    blog: 'Blog',
+    store: 'Tienda',
+    about: 'Asociación',
+    privacy: 'Política de privacidá',
+    terms: "Términos d'usu",
+    contact: 'Contactu',
+    tropeltxos: 'Tropeltxos',
+  },
+  auth: {
+    login: {
+      title: 'Bienveníu.',
+      link: '¿Yá tienes una cuenta? Anicia sesión',
+    },
+    signup: {
+      title: 'Únete al pelotón.',
+      link: '¿Nun tienes cuenta? Rexístrate',
+    },
+    passwordReset: {
+      title: 'Restablecer contraseña.',
+      intro:
+        '¿Esquéciste la to contraseña? Escribe la to direición de corréu y unviarémoste un enllaz pa restablecela.',
+      link: '¿Has olvidáu la to contraseña?',
+    },
+    passwordResetConfirm: {
+      intro: 'Escribe equí la to nueva contraseña; podrás usala pa acceder a Tropela.',
+    },
+  },
+  countdown: {
+    months: 'mes | meses',
+    days: 'día | díes',
+    hours: 'hora | hores',
+    minutes: 'minutu | minutos',
+    seconds: 'segundu | segundos',
+  },
+  season: {
+    allRaces: 'Toles competiciones',
+    calendar: 'Calendariu',
+    nextRace: 'Próxima competición | Próximes competiciones',
+    menu: {
+      standings: 'Clasificaciones de la temporada',
+    },
+    collection: 'Colección | Colecciones',
+    race: 'Competición | Competiciones',
+    racesCount: 'Nun hai competiciones | Una competición | {n} competiciones',
+    performance: 'Rendimientu',
+  },
+  race: {
+    menu: {
+      presentation: 'Presentación',
+      standings: 'Clasificaciones',
+      standings2: 'Clasificaciones (hasta esta carrera)',
+      points: 'Puntos de los ciclistes',
+      dorsals: 'Dorsales',
+      news: 'Noticies',
+      createPorra: 'Facer equipu',
+      myPorra: 'Mi equipu',
+    },
+    canceled: 'Cancelada',
+    stage: 'Etapa | Etapes',
+    stage_today: 'Etapa de güei | Etapes de güei',
+    team: 'Equipu | Equipos',
+    dorsal: {
+      filter: 'Filtrar por nome, equipu o país',
+      group: 'Grupu | Grupos',
+      status: 'Estáu | Estáos',
+      statuses: {
+        unconfirmed: 'Ensin confirmar',
+        participating: 'Confirmáu',
+        notParticipating: 'Nun participa',
+        retired: 'Retiráu',
+      },
+      sort: {
+        alpha: 'Ordenar por nome',
+        percent: 'Ordenar por porcentaxe',
+        position: 'Ordenar por puntos',
+      },
+    },
+    porra: {
+      registrationOpens: 'Abrimos en',
+      registrationOpenFor: "Fai'l to equipu en",
+      showAllStages: 'Amosar toles etapes',
+    },
+    porrasCount: 'Nun hai equipos | Un equipu | {n} equipos',
+    pointsRules: 'Puntuaciones',
+  },
+  place: {
+    menu: {
+      standings: 'Clasificaciones locales',
+    },
+  },
+  game: {
+    menu: {
+      standings: 'Clasificaciones patrocinaes',
+    },
+    banner: 'Esta ye una competición patrocina, con premios pa los usuarios de Tropela.',
+    legal: 'Bases llexales',
+    prizes: 'Premios',
+  },
+  user: {
+    menu: {
+      admin: "Zona d'aministración",
+      avatar: 'Mi ciclista',
+      dashboard: 'Mis estadístiques',
+      privacy: 'Privacidá',
+      profile: 'Perfil',
+      settings: 'Configuración',
+      settingsAndPrivacy: 'Configuración y privacidá',
+      notifications: 'Notificaciones',
+      subscriptions: 'Suscripciones',
+      login: 'Aniciar sesión',
+      logout: 'Zarrar sesión',
+      passwordChange: 'Contraseña',
+      emails: 'Emails',
+      socialConnections: 'Conexones sociales',
+      signup: 'Rexistrase',
+      standings: 'Mis clasificaciones',
+      tropeltxos: 'Mis tropeltxos',
+    },
+    change_language:
+      'Pues camudar el idioma de la aplicación equí. El cambéu aplicarase de siguío y guardarase nel to perfil, pa que se recuerde la próxima vez que entres (en cualquier dispositivu).',
+    confirm: {
+      logout: '¿Tas seguru de que quies zarrar sesión?',
+    },
+    tropeltxo: 'Tropeltxo | Tropeltxos',
+  },
+  ui: {
+    form: {
+      actions: {
+        change: 'Cambiar',
+        create: 'Crear',
+        delete: 'Esborrar',
+        update: 'Actualizar',
+        refresh: 'Actualizar',
+        reset: 'Resetear',
+        save: 'Guardar',
+        submit: 'Unviar',
+        filter: 'Filtrar',
+        dismiss: 'Descartar',
+        typeMessage: 'Escribe el to mensaxe',
+        login: 'Aniciar sesión',
+        signup: 'Rexístrate',
+        downloadIcs: 'Descargar calendariu',
+      },
+      fields: {
+        username: "Nome d'usuariu",
+        password: 'Contraseña',
+        password2: 'Repit la contraseña',
+        email: 'Corréu',
+        firstName: 'Nome',
+        lastName: 'Apellíos',
+        city: 'Ciudá',
+        flag: 'La to bandera',
+      },
+      hints: {
+        city: 'Emplegaremos esto pa les clasificaciones locales.',
+        flag: 'Amuésase nes clasificaciones.',
+      },
+      required: 'Obligatoriu.',
+      rules: {
+        required: 'Esti campu ye obligatoriu.',
+      },
+      social: {
+        facebook: 'Continuar con Facebook',
+        google: 'Continuar con Google',
+        twitter: 'Continuar con Twitter / X',
+      },
+    },
+    select: {
+      all: 'To | Todos',
+      aggregate: 'Xeneral',
+      locale: 'Idioma',
+      race: 'Competición',
+      stage: 'Etapa',
+      finalStage: 'Final',
+      annual: 'Anual',
+    },
+    nav: {
+      pageFirst: 'Primera páxina',
+      pagePrevious: 'Páxina anterior',
+      pageNext: 'Páxina siguiente',
+      pageLast: 'Cabera páxina',
+    },
+    or: 'o',
+  },
+  message: {
+    invalidCredentials: "El nome d'usuariu y/o la contraseña que has especificáu nun son correutos.",
+    loggedIn: 'Entresti como {username}',
+    loggedOut: 'Has zarráu sesión',
+    passwordResetRequested: 'Unvióse un corréu pa restablecer la contraseña',
+    porraSaveDelay: 'La to porra guardarase en {seconds} segundos',
+    porraCreated: 'La to porra creóse correutamente',
+    porraUpdated: 'La to porra actualizóse',
+    porraRandomSelected: 'Propúnxose una porra aleatoria',
+    searchDelay: 'La búsqueda realizaráse en {seconds} segundos',
+    userProfileUpdated: 'El to perfil actualizóse',
+  },
+  notification: {
+    makePorra: '¡Nun escaezas facer el to equipu pa {race}!',
+  },
+  premium: {
+    banner: {
+      join: 'Únete a los Gregarios de lluxu y disfruta de ventaxes exclusives.',
+      noAds: '¡Y ensin publicidá!',
+    },
+    action: {
+      upgrade: 'Faite Gregariu',
+    },
+  },
+  privacy: {
+    shareOnSocialMedia: 'Compartir la to actividá en redes sociales',
+    shareOnSocialMediaDescription:
+      'Cuando faigas un equipu, puedes compartir en redes sociales que tas participando (si accedes vía Twitter o Facebook).',
+    makeProfilePublic: 'Facer públicu el to perfil',
+    makeProfilePublicDescription: 'Puedes facer públicu el to perfil y compartir los tos resultaos y estadístiques.',
+    receiveEmails: 'Recibir correos de Tropela',
+    receiveEmailsDescription: 'Recordatorios, novedaes, etc.',
+    receiveNotifications: 'Recibir notificaciones',
+    receiveNotificationsDescription:
+      'Recibir notificaciones `push` en dispositivos móviles, cuando abrimos una nueva competición.',
+  },
+  faq: {
+    title: 'Preguntes frecuentes',
+    helpTropela: {
+      q: '¿Cómo puedo ayudar a Tropela?',
+      description: 'Faite Gregariu de Lluxu pa apoyar los nuestros proyectos.',
+    },
+    translateTropela: {
+      q: '¿Puedo ayudar coles traducciones?',
+      description:
+        'Apoyamos les llingües minoritaries y aquelles falaes en países con una fuerte cultura ciclista. Puedes amestar nuevos idiomes o meyorar les traducciones a través del nuesu repositoriu públicu.',
+    },
+    dashboardPercentage: {
+      q: '¿Qué signifiquen los porcentaxes nel to dashboard?',
+      description:
+        "Nel to Tropela, los porcentaxes indiquen el to rendimientu en cada competición. El porcentaxe ye el porcentaxe d'usuarios que terminaron detrás de ti.",
+    },
+    profileAvatar: {
+      q: '¿Cómo puedo cambiar el mi ciclista?',
+      description: 'Puedes cambiar el to ciclista nel to perfil. ¡Tienes cientos de combinaciones posibles!',
+    },
+    profilePremium: {
+      q: '¿Por qué dalgunos usuarios tienen esti iconu?',
+      description: 'Los Gregarios de lluxu son usuarios que ayuden a Tropela, y disfruten de ventaxes exclusives.',
+    },
+    raceLoginToCreatePorra: {
+      q: '¿Cómo puedo participar nesta competición?',
+      description: 'Pa participar nesta competición, tienes que aniciar sesión y facer la to porra.',
+    },
+    raceDorsalsRetired: {
+      q: '¿Dónde puedo ver los dorsales retiraos?',
+      description: 'Puedes filtrar los dorsales por estáu pa ver los retiraos.',
+    },
+    racePointsAbc: {
+      q: '¿Qué signifiquen les lletres que apaecen xunto a los dorsales?',
+      description:
+        'Les lletres amuesen cuántos usuarios seleccionaron a los ciclistes. Hai cuatro lletres: A (>%30), B (%30-%3), C (%3-%1) y D (<%1).',
+    },
+    racePointsStars: {
+      q: '¿Qué quier dicir les estrelles xunto a los ciclistes?',
+      description:
+        'Los ciclistes con estrelles tán na porra ideal; ye dicir, son la meyor opción pa ganar esta competición.',
+    },
+    raceStandings: {
+      q: '¿Cómo se calculen les clasificaciones?',
+      description:
+        'Al final de cada etapa, recibirás los puntos de los ciclistes que seleccionasti na to porra. Estos puntos son los que determinen les clasificaciones.',
+    },
+    racePlaceStandings: {
+      q: '¿Qué puedo facer si nun veo les clasificaciones locales?',
+      description: 'Pa ver les clasificaciones locales, tienes que especificar la to ciudá nel to perfil.',
+    },
+    racePorraRiskFactor: {
+      q: "¿Cuál ye'l significáu de la barra?",
+      description: "La barra indica'l nivel de riesgu de 0 a 10: el riesgu que l'usuariu tomó al facer la so porra.",
+    },
+    racePorraMyRiskFactor: {
+      q: "¿Cuál ye'l nivel de riesgu de la mio porra?",
+      description: "El riesgu que l'usuariu tomó al facer la so porra.",
+    },
+    racePorraAds: {
+      q: '¿Por qué veo anuncios antes de guardar la mi porra?',
+      description: 'Sólo los Gregarios de lluxu pueden guardar la so porra ensin ver anuncios.',
+    },
+    racePorraCreateRandom: {
+      q: '¿Podeis facer una porra aleatoria pa mi?',
+      description: 'Por supuesto. ¡Elige les lletres que debemos tener en cuenta y te propondremos una porra!',
+    },
+    seasonIcs: {
+      q: '¿Cómo puedo recibir el calendariu de competiciones?',
+      description:
+        'Con esti enllaz pues descargar un ficheru ICS pa amestar les competiciones de la temporada al to calendariu dixital.',
+    },
+    tropeltxos: {
+      q: '¿Cómo puedo xestionar los tropeltxos?',
+      description:
+        'Con los tropeltxos podrás ver subclasificaciones con los tos amigos, familiares, compañeros de trabayu o vecinos.',
+    },
+    tropeltxoPrize: {
+      q: '¿Hai premios pal ganador del tropeltxo?',
+      description:
+        'Tropela nun ofrece premios nos tropeltxos, pero nun escaezas que tenemos una tienda si quies repartir premios!',
+    },
+  },
 };
--- gl/index.ts
+++ gl/index.ts
@@ -1,6 +1,317 @@
 //! tropela-app translation file
 //! locale : Galician [gl]
+//! author : github : https://github.com/github
 
 export default {
   updated: 'Actualizamos a aplicación. Refresca para usar a última versión.',
+  welcome: 'En Tropela.eus podes competir cos teus familiares e amigos nas mellores carreiras ciclistas da tempada.',
+  menu: {
+    home: 'Inicio',
+    races: 'Competicións',
+    seasons: 'Tempadas',
+    rules: 'Regras',
+    premium: 'Gregario de luxo | Gregarios de luxo',
+    blog: 'Blog',
+    store: 'Tenda',
+    about: 'Asociación',
+    privacy: 'Política de privacidade',
+    terms: 'Termos de uso',
+    contact: 'Contacto',
+    tropeltxos: 'Tropeltxos',
+  },
+  auth: {
+    login: {
+      title: 'Benvido.',
+      link: 'Xa tes unha conta? Inicia sesión',
+    },
+    signup: {
+      title: 'Únete ao pelotón.',
+      link: 'Non tes conta? Rexístrate',
+    },
+    passwordReset: {
+      title: 'Restablecer contrasinal.',
+      intro:
+        'Esqueciches o teu contrasinal? Escribe o teu enderezo de correo electrónico e enviarémosche unha ligazón para restablecelo.',
+      link: 'Esqueciches o teu contrasinal?',
+    },
+    passwordResetConfirm: {
+      intro: 'Escribe aquí o teu novo contrasinal; poderás usalo para acceder a Tropela.',
+    },
+  },
+  countdown: {
+    months: 'mes | meses',
+    days: 'día | días',
+    hours: 'hora | horas',
+    minutes: 'minuto | minutos',
+    seconds: 'segundo | segundos',
+  },
+  season: {
+    allRaces: 'Todas as competicións',
+    calendar: 'Calendario',
+    nextRace: 'Próxima competición | Próximas competicións',
+    menu: {
+      standings: 'Clasificacións da tempada',
+    },
+    collection: 'Colección | Coleccións',
+    race: 'Competición | Competicións',
+    racesCount: 'Non hai competicións | Unha competición | {n} competicións',
+    performance: 'Rendemento',
+  },
+  race: {
+    menu: {
+      presentation: 'Presentación',
+      standings: 'Clasificacións',
+      standings2: 'Clasificacións (ata esta carreira)',
+      points: 'Puntos dos ciclistas',
+      dorsals: 'Dorsais',
+      news: 'Novas',
+      createPorra: 'Fai equipo',
+      myPorra: 'O meu equipo',
+    },
+    canceled: 'Cancelada',
+    stage: 'Etapa | Etapas',
+    stage_today: 'Etapa de hoxe | Etapas de hoxe',
+    team: 'Equipo | Equipos',
+    dorsal: {
+      filter: 'Filtrar por nome, equipo ou país',
+      group: 'Grupo | Grupos',
+      status: 'Estado | Estados',
+      statuses: {
+        unconfirmed: 'Sen confirmar',
+        participating: 'Confirmado',
+        notParticipating: 'Non participa',
+        retired: 'Retirado',
+      },
+      sort: {
+        alpha: 'Ordenar por nome',
+        percent: 'Ordenar por porcentaxe',
+        position: 'Ordenar por puntos',
+      },
+    },
+    porra: {
+      registrationOpens: 'Abrimos en',
+      registrationOpenFor: 'Fai o teu equipo en',
+      showAllStages: 'Mostrar todas as etapas',
+    },
+    porrasCount: 'Non hai equipos | Un equipo | {n} equipos',
+    pointsRules: 'Puntuacións',
+  },
+  place: {
+    menu: {
+      standings: 'Clasificacións locais',
+    },
+  },
+  game: {
+    menu: {
+      standings: 'Clasificacións patrocinadas',
+    },
+    banner: 'Esta é unha competición patrocinada, con premios para os usuarios de Tropela.',
+    legal: 'Bases legais',
+    prizes: 'Premios',
+  },
+  user: {
+    menu: {
+      admin: 'Zona de administración',
+      avatar: 'O meu ciclista',
+      dashboard: 'As miñas estatísticas',
+      privacy: 'Privacidade',
+      profile: 'Perfil',
+      settings: 'Configuración',
+      settingsAndPrivacy: 'Configuración e privacidade',
+      notifications: 'Notificacións',
+      subscriptions: 'Subscripcións',
+      login: 'Iniciar sesión',
+      logout: 'Pechar sesión',
+      passwordChange: 'Contrasinal',
+      emails: 'Emails',
+      socialConnections: 'Conexións sociais',
+      signup: 'Rexistrarse',
+      standings: 'As miñas clasificacións',
+      tropeltxos: 'Os meus tropeltxos',
+    },
+    change_language:
+      'Podes cambiar o idioma da aplicación aquí. O cambio aplicarase inmediatamente e gardarase no teu perfil, para que se recorde a próxima vez que inicies sesión (en calquera dispositivo).',
+    confirm: {
+      logout: 'Estás seguro de que queres pechar sesión?',
+    },
+    tropeltxo: 'Tropeltxo | Tropeltxos',
+  },
+  ui: {
+    form: {
+      actions: {
+        change: 'Cambiar',
+        create: 'Crear',
+        delete: 'Borrar',
+        update: 'Actualizar',
+        refresh: 'Actualizar',
+        reset: 'Restablecer',
+        save: 'Gardar',
+        submit: 'Enviar',
+        filter: 'Filtrar',
+        dismiss: 'Descartar',
+        typeMessage: 'Escribe a túa mensaxe',
+        login: 'Iniciar sesión',
+        signup: 'Rexístrate',
+        downloadIcs: 'Descargar calendario',
+      },
+      fields: {
+        username: 'Nome de usuario',
+        password: 'Contrasinal',
+        password2: 'Repite o contrasinal',
+        email: 'Email',
+        firstName: 'Nome',
+        lastName: 'Apelidos',
+        city: 'Cidade',
+        flag: 'A túa bandeira',
+      },
+      hints: {
+        city: 'Empregaremos isto para as clasificacións locais.',
+        flag: 'Amósase nas clasificacións.',
+      },
+      required: 'Obrigatorio.',
+      rules: {
+        required: 'Este campo é obrigatorio.',
+      },
+      social: {
+        facebook: 'Continuar con Facebook',
+        google: 'Continuar con Google',
+        twitter: 'Continuar con Twitter / X',
+      },
+    },
+    select: {
+      all: 'Todo | Todos',
+      aggregate: 'Xeral',
+      locale: 'Idioma',
+      race: 'Competición',
+      stage: 'Etapa',
+      finalStage: 'Final',
+      annual: 'Anual',
+    },
+    nav: {
+      pageFirst: 'Primeira páxina',
+      pagePrevious: 'Páxina anterior',
+      pageNext: 'Páxina seguinte',
+      pageLast: 'Última páxina',
+    },
+    or: 'ou',
+  },
+  message: {
+    invalidCredentials: 'O nome de usuario e/ou o contrasinal que especificaches non son correctos.',
+    loggedIn: 'Iniciaste sesión como {username}',
+    loggedOut: 'Pechaches sesión',
+    passwordResetRequested: 'Enviouse un email para restablecer o contrasinal',
+    porraSaveDelay: 'A túa porra gardarase en {seconds} segundos',
+    porraCreated: 'A túa porra creouse correctamente',
+    porraUpdated: 'A túa porra actualizouse',
+    porraRandomSelected: 'Propuñéronse unha porra aleatoria',
+    searchDelay: 'A busca realizarase en {seconds} segundos',
+    userProfileUpdated: 'O teu perfil actualizouse',
+  },
+  notification: {
+    makePorra: 'Non esquezas facer o teu equipo para {race}!',
+  },
+  premium: {
+    banner: {
+      join: 'Únete aos Gregarios de luxo e goza de vantaxes exclusivas.',
+      noAds: 'E sen publicidade!',
+    },
+    action: {
+      upgrade: 'Fai Gregario',
+    },
+  },
+  privacy: {
+    shareOnSocialMedia: 'Compartir a túa actividade en redes sociais',
+    shareOnSocialMediaDescription:
+      'Cando creas un equipo, podes compartir en redes sociais que estás participando (se accedes via Twitter ou Facebook).',
+    makeProfilePublic: 'Fai público o teu perfil',
+    makeProfilePublicDescription: 'Podes facer público o teu perfil e compartir os teus resultados e estatísticas.',
+    receiveEmails: 'Recibir emails de Tropela',
+    receiveEmailsDescription: 'Recordatorios, novidades, etc.',
+    receiveNotifications: 'Recibir notificacións',
+    receiveNotificationsDescription:
+      'Recibir notificacións `push` en dispositivos móbiles, cando abrimos unha nova competición.',
+  },
+  faq: {
+    title: 'Preguntas frecuentes',
+    helpTropela: {
+      q: 'Como podo axudar a Tropela?',
+      description: 'Fai Gregario de Lujo para apoiar os nosos proxectos.',
+    },
+    translateTropela: {
+      q: 'Podo axudar coas traducións?',
+      description:
+        'Apoiamos as linguas minoritarias e aquelas faladas en países cunha forte cultura ciclista. Podes engadir novos idiomas ou mellorar as traducións a través do noso repositorio público.',
+    },
+    dashboardPercentage: {
+      q: 'Que significan as porcentaxes no teu dashboard?',
+      description:
+        'No teu Tropela, as porcentaxes indican o teu rendemento en cada competición. A porcentaxe é a porcentaxe de usuarios que remataron por detrás de ti.',
+    },
+    profileAvatar: {
+      q: 'Como podo cambiar o meu ciclista?',
+      description: 'Podes cambiar o teu ciclista no teu perfil. Tes centos de combinacións posibles!',
+    },
+    profilePremium: {
+      q: 'Por que algúns usuarios teñen este icono?',
+      description: 'Os Gregarios de luxo son usuarios que axudan a Tropela, e gozan de vantaxes exclusivas.',
+    },
+    raceLoginToCreatePorra: {
+      q: 'Como podo participar nesta competición?',
+      description: 'Para participar nesta competición, debes iniciar sesión e facer a túa porra.',
+    },
+    raceDorsalsRetired: {
+      q: 'Onde podo ver os dorsais retirados?',
+      description: 'Podes filtrar os dorsais por estado para ver os retirados.',
+    },
+    racePointsAbc: {
+      q: 'Que significan as letras que aparecen xunto aos dorsais?',
+      description:
+        'As letras amosan cantos usuarios seleccionaron aos ciclistas. Hai catro letras: A (>%30), B (%30-%3), C (%3-%1) e D (<%1).',
+    },
+    racePointsStars: {
+      q: 'Que queren dicir as estrelas xunto aos ciclistas?',
+      description:
+        'Os ciclistas con estrelas están na porra ideal; é dicir, son a mellor opción para gañar esta competición.',
+    },
+    raceStandings: {
+      q: 'Como se calculan as clasificacións?',
+      description:
+        'Ao final de cada etapa, recibirás os puntos dos ciclistas que seleccionaches na túa porra. Estes puntos son os que determinan as clasificacións.',
+    },
+    racePlaceStandings: {
+      q: 'Que podo facer se non vexo as clasificacións locais?',
+      description: 'Para ver as clasificacións locais, debes especificar a túa cidade no teu perfil.',
+    },
+    racePorraRiskFactor: {
+      q: 'Cal é o significado da barra?',
+      description: 'A barra indica o nivel de risco de 0 a 10: o risco que o usuario tomou ao facer a súa porra.',
+    },
+    racePorraMyRiskFactor: {
+      q: 'Cal é o nivel de risco da miña porra?',
+      description: 'O risco que tomaches ao facer a túa porra, de 0 a 10:',
+    },
+    racePorraAds: {
+      q: 'Por que vexo anuncios antes de gardar a miña porra?',
+      description: 'Só os Gregarios de luxo poden gardar a súa porra sen ver anuncios.',
+    },
+    racePorraCreateRandom: {
+      q: 'Podedes facer unha porra aleatoria para min?',
+      description: 'Por suposto. Escolle as letras que debemos ter en conta e propoñerémosche unha porra!',
+    },
+    seasonIcs: {
+      q: 'Como podo recibir o calendario de competicións?',
+      description:
+        'Co seguinte enlace podes descargar un ficheiro ICS co que podes engadir as competicións da tempada ao teu calendario dixital.',
+    },
+    tropeltxos: {
+      q: 'Onde podo xestionar os meus tropeltxos?',
+      description:
+        'Cos tropeltxos poderás ver subclasificacións cos teus amigos, familiares, compañeiros de traballo ou veciños.',
+    },
+    tropeltxoPrize: {
+      q: 'Hai premios para o gañador do tropeltxo?',
+      description:
+        'Tropela non ofrece premios nos tropeltxos, pero non esquezas que temos unha tenda se queres repartir premios!',
+    },
+  },
 };
